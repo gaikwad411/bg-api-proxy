408cd8f5eb0d82569807c3ccda5764f014eae4d5
Sachin Vilas Gaikwad <SGaikwad3@slb.com>
2019-01-17 11:38:13 +0530

    test framework added

---
+++ test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+
+describe('Hello World Test', function () {
+    // 
+    it('should say hi', function () {
+        expect('hi').to.be.equal('hi');
+    });
+});
